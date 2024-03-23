6e2d38ba1e96833b0dcf8e8017e89b37d51865d1
Sergei <aspdrago@gmail.com>
2024-03-23 17:04:24 +0800

    Type Safe Components

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,10 +1,24 @@
-import { View } from 'react-native';
-import products from '../../../assets/data/products';
-import ProductListItem from '../../components/ProductListItem';
+import { StyleSheet, FlatList } from 'react-native';
+import products from '@assets/data/products';
+import ProductListItem from '@components/ProductListItem';
 
 export default function MenuScreen() {
-  return (<View>
-    <ProductListItem product={products[0]} />
-    <ProductListItem product={products[1]} />
-  </View>);
+  return (
+    <FlatList data={products}
+    renderItem={({ item }) => <ProductListItem product={item} />}
+    numColumns={2}
+    contentContainerStyle={styles.contentContainer}
+    columnWrapperStyle={styles.columnWrapper}
+    />
+  );
 }
+
+const styles = StyleSheet.create({
+  contentContainer: {
+    gap: 10,
+    padding: 10,
+  },
+  columnWrapper: {
+    gap: 10,
+  }
+});
--- src/components/ProductListItem.tsx
+++ src/components/ProductListItem.tsx
@@ -1,10 +1,22 @@
 import { StyleSheet, Text, View, Image } from 'react-native';
-import Colors from '../constants/Colors';
+import Colors from '@/constants/Colors';
+import { Product } from '@/types';
 
-const ProductListItem = ({ product }) => {
+export const defaultPizzaImage = 
+    'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/default.png';
+
+type ProductListItemProps = {
+    product: Product;
+}
+
+const ProductListItem = ({ product }: ProductListItemProps) => {
     return (
       <View style={styles.container}>
-        <Image source={{ uri: product.image }} style={styles.image} />    
+        <Image source={{ uri: product.image || defaultPizzaImage }}
+        style={styles.image}
+        resizeMode="contain"
+        />  
+
         <Text style={styles.title}>{product.name}</Text>
         <Text style={styles.price}>${product.price}</Text>
       </View>
@@ -18,6 +30,8 @@ const styles = StyleSheet.create({
       backgroundColor: 'white',
       padding: 10,
       borderRadius: 10,
+      flex: 1,
+      maxWidth: '50%',
     },
     image: {
       width: '100%',
