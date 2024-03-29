0d9b5b756125ab4eb8b97576df715cf75d80543f
Sergei <aspdrago@gmail.com>
2024-03-29 23:22:10 +0800

    Tabs buttons have beed implemented

--- src/app/(tabs)/menu/[id].tsx
+++ src/app/(tabs)/menu/[id].tsx
@@ -1,11 +1,12 @@
 import { View, Text } from 'react-native';
 import React from 'react';
-import { useLocalSearchParams } from 'expo-router';
+import {Stack, useLocalSearchParams } from 'expo-router';
 
 const ProductDetailsScreen = () => {
     const { id } = useLocalSearchParams();
     return (
         <View>
+            <Stack.Screen options={{ title: 'Details' + id, headerTitleAlign: 'center' }} />
             <Text style={{ fontSize: 20 }}>ProductDetailsScreen for id: {id}</Text>
         </View>
     );
