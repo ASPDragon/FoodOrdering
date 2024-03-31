f86ab18cc86887686b1614eae3f5474c45bc0424
Sergei <aspdrago@gmail.com>
2024-03-31 13:45:15 +0800

    Additional Shopping Cart Features

--- src/app/cart.tsx
+++ src/app/cart.tsx
@@ -1,21 +1,41 @@
-import {View, Text, Platform, FlatList} from 'react-native';
+import { View, Text, Platform, FlatList, StyleSheet } from 'react-native';
 import { StatusBar } from "expo-status-bar";
 import { useCart } from "@/providers/CartProvider";
 import CartListItem from "@components/CartListItem";
+import Button from "@components/Button";
+import {Styles} from "@expo/config-plugins/build/android";
+import {container} from "ansi-fragments";
 
 const CartScreen = () => {
-    const { items } = useCart();
+    const { items, total } = useCart();
 
     return (
-        <View>
+        <View style={styles.container}>
             <FlatList
                 data={items}
                 renderItem={({item}) => <CartListItem cartItem={item} />}
-                contentContainerStyle={{ padding: 10, gap: 10 }}
+                contentContainerStyle={{ gap: 10 }}
             />
+            
+            <Text style={styles.total}>
+                Total: ${total}
+            </Text>
+            <Button text="Checkout" />
+            
             <StatusBar style={Platform.OS === 'ios' ? 'light' : 'auto'} />
         </View>
     );
 }
 
 export default CartScreen;
+
+const styles = StyleSheet.create({
+    container: {
+        padding: 10,
+    },
+    total: {
+        marginTop: 20,
+        fontSize: 20,
+        fontWeight: '500',
+    }
+});
